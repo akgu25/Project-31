8fc7333d2681d63fbe38e7b4520952d220b67249
akgu25 <78292653+akgu25@users.noreply.github.com>
2021-05-24 18:40:05 +0700

    Add files via upload

---
+++ Particle.js
@@ -0,0 +1,16 @@
+class Divisions{
+    constructor(x,y,this.r) {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.circle(x,y,this.r,options);
+      this.color = color(random(0,255),random(0,255), random(0,255)); 
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("white");
+    rect(pos.x, pos.y, this.w,this.h);
+    }
+  };
